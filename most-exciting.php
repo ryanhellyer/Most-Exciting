c9fc73c23f56f88c7f1fc96bdaa0b9e5b4aa1e8e
Ryan Hellyer <ryan@pixopoint.com>
2013-03-30 08:08:39 +0100

    Most happy

--- most-exciting.php
+++ most-exciting.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * Plugin Name: Most Exciting
+ * Plugin Name: Most Happy
  * Plugin URI: trepmal.com
- * Description: Make WordPress more exciting
- * Version: 2013.03.29
- * Author: Kailey Lampert
+ * Description: Make WordPress more happy
+ * Version: 2013.03.30
+ * Author: Kailey Lampert and Ryan Hellyer
  * Author URI: kaileylampert.com
  * License: GPLv2 or later
  * TextDomain:
@@ -12,8 +12,7 @@
  * Network: false
  */
 
-add_filter( 'gettext', 'most_exciting', 99 );
-function most_exciting( $t ) {
-	return "$t!!";
+add_filter( 'gettext', 'most_happy', 99 );
+function most_happy( $t ) {
+	return "$t :)";
 }
-//eof
